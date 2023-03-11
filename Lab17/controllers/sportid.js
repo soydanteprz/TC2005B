495b57adf0f3d19ca1341943da57940ba39273ed
Dante <ddperezp03@gmail.com>
2023-03-11 16:58:23 -0600

    Lab17 Done

---
+++ Lab17/controllers/sportid.js
@@ -0,0 +1,143 @@
+const Sportid = require('../models/sportid');
+const user = require('../models/users');
+
+const playersnfl = [
+    {
+        name: 'Tom Brady',
+        team: 'Tampa Bay Buccaneers',
+        image: 'https://static.www.nfl.com/image/private/t_headshot_desktop/league/q7dpdlxyu5rs05rgh1le'
+    },
+    {
+        name: 'Patrick Mahomes',
+        team: 'Kansas City Chiefs',
+        image: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3139477.png&w=350&h=254'
+    },
+    {
+        name: 'Aaron Rodgers',
+        team: 'Green Bay Packers',
+        image: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/8439.png'
+    }
+];
+
+exports.get_login = (req, res, next) => {
+    res.render('login', {
+        pageTitle: 'Login',
+        users: user.fetch(),
+        user: req.session.user || '',
+    });
+};
+
+exports.post_login = (req, res, next) => {
+    const userNew = new user({
+        username: req.body.username,
+        password: req.body.password,
+    });
+    userNew.save();
+    req.session.user = userNew.username;
+    res.status(300).redirect("/sportid");
+};
+
+
+exports.get_playersnba = (req, res, next) => {
+
+    if (req.session.user) {
+        console.log("User is logged in");
+        const cookies = req.get('Cookie') || '';
+        let consultas = cookies.split('=')[1] || 0;
+        consultas++;
+
+        res.setHeader('Set-Cookie', 'consultas=' + consultas + '; HttpOnly');
+
+        const id = req.params.id || 0;
+
+        Sportid.fetch(id)
+            .then(([rows, fieldData]) => {
+                console.log(rows);
+
+                res.render('nba', {
+                    pageTitle: 'NBA',
+                    players: rows,
+                    lastPlayer: req.session.lastPlayer || '',
+                });
+            })
+            .catch(err =>{
+                console.log(err);
+            });
+    } else {
+        console.log("User is not logged in");
+        res.redirect("/sportid/login");
+        // console.log("User is not logged in");
+        // res.redirect("/sportid/login");
+    }
+};
+
+exports.get_home = (req, res, next) => {
+
+    if (req.session.user) {
+        console.log("User is logged in");
+        res.render("home", { pageTitle: "Home", user: req.session.user });
+    } else {
+        console.log("User is not logged in");
+        res.redirect("/sportid/login");
+    }
+};
+
+exports.get_preguntas = (req, res, next) => {
+
+    if (req.session.user) {
+        console.log("User is logged in");
+        res.render("preguntas", { pageTitle: "Preguntas", user: req.session.user });
+    } else {
+        console.log("User is not logged in");
+        res.redirect("/sportid/login");
+    }
+};
+
+exports.get_playersnfl = (req, res, next) => {
+
+    if (req.session.user) {
+        console.log("User is logged in");
+        res.render("nfl", { pageTitle: "NFL", players: playersnfl });
+    } else {
+        console.log("User is not logged in");
+        res.redirect("/sportid/login");
+    }
+};
+
+
+
+exports.get_nuevo = (req, res, next) => {
+
+    if (req.session.user) {
+        console.log("User is logged in");
+        res.render("nuevo", { pageTitle: "Nuevo", user: req.session.user });
+    } else {
+        console.log("User is not logged in");
+        res.redirect("/sportid/login");
+    }
+};
+
+exports.post_nuevo = (req, res, next) => {
+
+    console.log(req.body);
+
+    const playernba = new Sportid({
+        name: req.body.name,
+        team: req.body.team,
+        image: req.body.image,
+    });
+
+    console.log(playernba);
+
+    playernba.save();
+    res.status(300).redirect("/sportid/nba");
+};
+
+
+
+
+
+
+
+
+
---
+++ Lab17/routes/sportid.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+const sportidController = require("../controllers/sportid");
+const usersController = require("../controllers/users");
+
+router.get('/nba/:id', sportidController.get_playersnba);
+
+router.get("/nba", sportidController.get_playersnba);
+
+router.get('/nuevo', sportidController.get_nuevo);
+
+router.post('/nuevo', sportidController.post_nuevo);
+
+router.get("/", sportidController.get_home);
+
+router.get("/nfl", sportidController.get_playersnfl);
+
+router.get("/preguntas", sportidController.get_preguntas);
+
+router.get('/login',sportidController.get_login);
+
+router.post('/login',sportidController.post_login);
+
+router.post('/logout',usersController.logout);
+
+module.exports = router;
